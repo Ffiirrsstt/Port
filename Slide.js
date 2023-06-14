bc75e9f54618b92d4e66aa060e69704feeaa9ad2
Ffiirrsstt <first.engineer15@gmail.com>
2023-06-14 07:40:48 +0700

    Update the portfolio
    Edit the image

--- Slide.js
+++ Slide.js
@@ -26,9 +26,10 @@ const message1 =
   "รูปภาพขณะเป็นตัวแทนประจำเซคเรียนขึ้นนำเสนอผลงานในงานสัมมนาและงานนำเสนอผลงาน 'make Life ให้ Beautiful' โดยมีวิทยากรเป็นคุณ 'ณัฐชา คุ้มวงษ์ดี' (Senior markeing executive บริษัทเอฟเวอร์พิงค์)";
 
 const linkImage1 = [
-  "https://scontent.fbkk29-5.fna.fbcdn.net/v/t39.30808-6/345654175_567335771933198_532647323781512923_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeFEfi8uQjDVstk3wkJ0CSj-DSypacJjkJINLKlpwmOQkmlQOZgW-cp7tT77vhkMcqmU1OrL2B7i5nKCwLdGKs-r&_nc_ohc=mRa7_nC4Vb8AX8FX7pU&_nc_ht=scontent.fbkk29-5.fna&oh=00_AfCEdDIAVL5Z3Is-IkuKJJcdtj5JOCvSlxp_O7E6GWQCKw&oe=64874201",
-  "https://scontent.fbkk29-5.fna.fbcdn.net/v/t39.30808-6/345876150_3408829226023395_6892781101989006146_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeFVG6Cmh8_ONrsqasSEX62pGXnBDw30Ug0ZecEPDfRSDf__Bc1-jXctQe4KneV6vDNh-mq7jcNEgFD5u4wFEcmq&_nc_ohc=2cq1mk6LIR0AX9pO7eN&_nc_ht=scontent.fbkk29-5.fna&oh=00_AfBAVU3mvpczj0uUiwEfIOufTjyYyhYcsE5zcmL8mg7UmQ&oe=6487F036",
-  "https://scontent.fbkk22-6.fna.fbcdn.net/v/t1.15752-9/346112974_288147196985275_6595936766585058630_n.png?_nc_cat=104&ccb=1-7&_nc_sid=ae9488&_nc_eui2=AeFS3LO-SVCEpfkIRlrzgyd_TlDwocrLeR1OUPChyst5HY2EU-NvSFoX-y5B-tn1gGxKTSxtq8CnJay74Fl4i3yF&_nc_ohc=M3lsKG8uI9AAX-2RHsT&_nc_ht=scontent.fbkk22-6.fna&oh=03_AdS1oE2R9GBbkQoTqfah3qobu7Lp5lAyK5Z8oJHiXQ7ZPg&oe=6491F23E",
+  "/img1/51486.jpg",
+  "/img1/51484.jpg",
+  "/img1/51485.jpg",
+  "/img1/51483.jpg",
 ];
 
 const message2 =
